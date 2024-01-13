d05f0d7996de4d08d7267350284ccf68a70c9812
makunouti <vandaru3@gmail.com>
2024-01-13 13:02:24 +0000

    Initial Commit

---
+++ 変数の定義/main.java
@@ -0,0 +1,17 @@
+package 変数の定義;
+
+class Main {
+    public static void main(String[] args) {
+        int number;
+
+        number = 3;
+
+        System.out.println(number);
+
+        String name;
+
+        name = "wanko";
+
+        System.out.println(name);
+    }
+}
---
+++ 変数の更新/main.java
@@ -0,0 +1,19 @@
+package 変数の更新;
+
+public class main {
+    public static main(String[] args) {
+        int number = 11;
+        String text = "Ruby";
+        System.out.println(number);
+        System.out.println(text);
+
+        number = 9;
+
+        System.out.println(number);
+
+        text = "Java";
+
+        System.out.println(text);
+    }
+    
+}
