0a4355a25dce5dcf71ee1c865fa4461c49d5f643
makunouti <vandaru3@gmail.com>
2024-01-14 21:27:06 +0000

    修正

--- if文/main.java
+++ if文/main.java
@@ -1,10 +1,11 @@
-package main;
+package if文;
 
-public class Main {
-    int a = 3;
+public class main {
+    public static void main(String[] args) {
+        int a = 3;
 
-    if (a == 3) {
-        System.out.println("a is 3.");
+        if (a == 3) {
+            System.out.println("a is 3");
+        }
     }
-    
 }
