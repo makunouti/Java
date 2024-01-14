9ce7d3e1dff1064a4e9c90ba79b7e01cdb1636ea
makunouti <vandaru3@gmail.com>
2024-01-14 21:36:42 +0000

    修正

---
+++ 繰り返し/main.java
@@ -0,0 +1,11 @@
+package 繰り返し;
+
+public class main {
+    public static void main(String[] args){
+        int i;
+        for (i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+    
+}
