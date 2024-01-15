277d6e7c2aaf0888cb4bd8905a4a5be1390278c9
makunouti <vandaru3@gmail.com>
2024-01-15 18:32:05 +0000

    1/16

---
+++ switch文/main.java
@@ -0,0 +1,21 @@
+package switch文;
+
+public class main {
+    public static void main(String[] args) {
+        int n = 3;
+        switch(n) {
+            case 1:
+                System.out.println("n is one");
+                break;
+            case 2:
+                System.out.println("n is two.");
+                break;
+            case 3:
+                System.out.println("n is tree.");
+                break;
+            default:
+                System.out.println("unkwon");
+                break;
+        }
+    }
+}
---
+++ 繰り返しwhile文/main.java
@@ -0,0 +1,17 @@
+package 繰り返しwhile文;
+
+public class main {
+    public static void main(String[] args) {
+        int i = 0;
+    //     while(i < 10) {
+    //         System.out.println(i);
+    //         i++;
+    //     }
+    // }
+    do {
+        System.out.println(i);
+        i++;
+    } while (i < 10);
+
+}
+}
