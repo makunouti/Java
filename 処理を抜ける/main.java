38f562fbdcd0fb10bac88d47a0118cd0047925ed
makunouti <vandaru3@gmail.com>
2024-01-16 12:22:10 +0000

    修正

--- 処理を抜ける/main.java
+++ 処理を抜ける/main.java
@@ -1,14 +1,14 @@
 package 処理を抜ける;
 
 public class main {
-    public static main(String[] args) {
+    public static void main(String[] args) {
         int i;
         for (i = 0; i < 10; i++) {
-            if(i == 5){
+            if (i == 5) {
                 break;
             }
-            System.out.println(i);  
+            System.out.println(i);
         }
-        System.out.println("finished!!");
+        System.out.println("finish!!");
     }
 }
