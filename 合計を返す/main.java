4eb86238f4f00055a108efdda72bde61e7fbe51c
makunouti <vandaru3@gmail.com>
2024-01-16 12:42:13 +0000

    合計を繰り返す

---
+++ 合計を返す/main.java
@@ -0,0 +1,10 @@
+package 合計を返す;
+
+public class main {
+    public static void main(String[] args){
+        System.out.println(add(3, 5));
+    }
+    static int add(int x, int y) {
+        return ( x + y);
+    }
+}
