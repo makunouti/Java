bf6bbc8ce4ce69ddd3caa10e97606025adfbadb6
makunouti <vandaru3@gmail.com>
2024-01-16 12:36:21 +0000

    処理を繰り返すを追加

---
+++ 処理を繰り返す/main.java
@@ -0,0 +1,14 @@
+package 処理を繰り返す;
+
+public class main {
+    public static void main(String[] args){
+        int i;
+        for(i = 0; i < 10; i++){
+            if( i == 5){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+    
+}
