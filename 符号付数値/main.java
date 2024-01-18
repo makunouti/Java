3b9778f15cab6bbb909c70f254c4fab060e2f3e6
makunouti <vandaru3@gmail.com>
2024-01-18 11:57:17 +0000

    符号付き数値

---
+++ 符号付数値/main.java
@@ -0,0 +1,12 @@
+package 符号付数値;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println(String.format("3桁の数字(符号付き):%1$+3d",1));
+        System.out.println(String.format("3桁の数字(符号付き):%1$+3d",123));
+        System.out.println(String.format("3桁の数字(符号付き):%1$+3d",12345));
+        System.out.println(String.format("3桁の数字(符号付き):%1$+3d",-1));
+        System.out.println(String.format("3桁の数字(符号付き):%1$+3d",-123));
+        System.out.println(String.format("3桁の数字(符号付き):%1$+3d",-12345));
+    }
+}
