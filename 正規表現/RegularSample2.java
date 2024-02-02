343d7d4f20611bf0f18279dbcb9acb9e6b322cbc
makunouti <vandaru3@gmail.com>
2024-02-02 20:34:42 +0000

    2/2

--- 正規表現/RegularSample2.java
+++ 正規表現/RegularSample2.java
@@ -3,7 +3,16 @@ package 正規表現;
 public class RegularSample2 {
     public static void main(String[] args) {
         String[] strs = {"a","aaa","bccd","bcaacd","fgcdaa","bcaacaad"};
-       
+        String reg = ".*a+.*"; 
+
+        for(int i = 0; i < strs.length; i++) {
+            if( strs[i].matches(reg)) {
+                System.out.println("文字列" + (i + 1) + ":\""
+                    + strs[i] + "\"は、正規表現\"" + reg + "\"にマッチする");
+            } else {
+                System.out.println("文字列" + (i + 1) + ":\""
+                    + strs[i] + "\"は、正規表現\"" + reg + "\"にマッチしない");
+            }
         }
     }
 }
