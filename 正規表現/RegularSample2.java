4e02d710eb4c8025102d3e914793d4a49da47549
makunouti <vandaru3@gmail.com>
2024-02-03 21:00:46 +0000

    2/3

--- 正規表現/RegularSample2.java
+++ 正規表現/RegularSample2.java
@@ -5,6 +5,14 @@ public class RegularSample2 {
         String[] strs = {"a","aaa","bccd","bcaacd","fgcdaa","bcaacaad"};
         String reg = ".*a+.*"; 
 
+        for(int i = 0; i < strs.length; i++) {
+            if( strs[i].matches(reg)) {
+                System.out.println("文字列" + (i + 1) + ":\""
+                    + strs[i] + "\"は、正規表現\"" + reg + "\"にマッチする");
+            } else {
+                System.out.println("文字列" + (i + 1) + ":\""
+                    + strs[i] + "\"は、正規表現\"" + reg + "\"にマッチしない");
+            }
         }
     }
 }
